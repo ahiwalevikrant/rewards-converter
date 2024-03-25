4e261cf1f5cf15d85775b48c171d0db051cdb378
ahiwalevikrant <vikrantahiwale71@gmail.com>
2024-03-25 02:35:14 -0400

    Reward class updated

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,26 +1,30 @@
 public class RewardValue {
-    private double cashValue;
-    private int milesValue;
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
-        this.cashValue=cashValue;
+        this.cashValue = cashValue;
+    }
+
+
+    public RewardValue(int mileValue) {
+        this.cashValue = convertToMiles(mileValue);
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
 
     }
 
-    public RewardValue(int milesValue){
-        this.milesValue=milesValue;
+    private static double convertToCash(int mileValue) {
+        return mileValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
-        public double getCashValue(){
+    public double getCashValue() {
         return cashValue;
-        }
-
-        public int getMilesValue(){
-        return milesValue;
     }
 
-
-
-
-
+    public double getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
 }
